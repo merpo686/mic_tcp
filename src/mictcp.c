71bd1592007534bd27e43a0bcda0e4a45260b1d8
Poitou Merlin <poitou@insa-20247>
2022-04-20 17:47:12 +0200

    4.2 pas finie, mais en bonne voie

--- src/mictcp.c
+++ src/mictcp.c
@@ -16,13 +16,24 @@ unsigned int PE=0;
 double pertes[10] ={1}; 
 //taux de pertes admissibles
 double perte_admi=0.2;
-/*  
- * Permet de créer un socket entre l’application et MIC-TCP
- * Retourne le descripteur du socket ou bien -1 en cas d'erreur
- */
+
+//4.1
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
+
+//4.2
+pthread_t tid_envoi;
+
+typedef struct buffer_send {
+    mic_tcp_payload messages[100];
+    int nb_messages;
+    int index;
+}buffer_send;
+
+struct buffer_send buf;
+
+void *sender(void * arg);
 
 int mic_tcp_socket(start_mode sm)
 {
@@ -30,7 +41,7 @@ int mic_tcp_socket(start_mode sm)
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
     start_m=sm;
     result = initialize_components(sm); /* Appel obligatoire */
-    set_loss_rate(51);
+    set_loss_rate(1);
     if (result!=-1)
     {
         sock.state=IDLE;
@@ -110,86 +121,113 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
         printf("ACK envoyé, connexion établie\n");
         addr_dest=addr;
         sock.state=ESTABLISHED;
+
+        //4.2: mise en place d'un buffer et du thread envoyeur
+        
+        buf.nb_messages=0;
+        buf.index=0;
+        pthread_create(&tid_envoi,NULL,sender,NULL);
         return sock.fd;
     }
     return -1;
 }
 
+void *sender(void * arg){
+    mic_tcp_pdu pdu={0};
+    while(1){
+        if (buf.nb_messages>0)
+        {
+            pthread_mutex_lock(&mutex);
+            pdu_global.header.source_port=sock.addr.port;
+            pdu_global.header.dest_port=addr_dest.port;
+            pdu_global.payload.size=buf.messages[buf.index].size;
+            pdu_global.payload.data=buf.messages[buf.index].data;
+            pthread_mutex_unlock(&mutex);
+            pdu_global.header.seq_num=PE;
+            pdu_global.header.ack='0';
+            pdu_global.header.ack_num=-1;
+            pdu_global.header.syn='0';
+            pdu_global.header.fin='0';
+            pertes[PE] = 0;
+            
+            // La regle du stop and wait est qu'une retransmission n'est seulement declenchee qu'au timeout et pas en reaction a une reception d'un PDU inapproprie
+            
+            int Timeout=0; // Timeout=-1 => il y a eu timeout
+            int nbRetransmit=5;
+            // on anticipe la perte pour la prise de déc
+            // precalcul du taux de reussite actuel ..
+            double sum=0;
+            for (int i=0; i<10;i++){
+                sum+=pertes[i];
+            }
+            int Retransmettre=(perte_admi + (sum)/10) < 1;// à 1 => retransmission necessaire 
+            printf("Estimation avant envoi dans le cas où l'on perd le paquet,\nTaux de perte : %f,Taux Livraison:%f/10 et Retransmettre=%d \n",1-sum/10,sum,Retransmettre);
+            
+            // Transmission PDU 
+            IP_send(pdu_global,addr_dest);
+            do {
+            Timeout=IP_recv(&pdu,&addr_dest,100);
+            if (Timeout == -1) { // il y a eu timeout
+            if (Retransmettre) {  // il y a necessite de retransmisison 
+                // Retransmission 
+                IP_send(pdu_global,addr_dest);
+                nbRetransmit--;
+                }
+            }
+            else // PDU recu
+            {
+                if (pdu.header.ack=='1' && pdu.header.ack_num==PE) { // c'est le bon ACK
+                pertes[PE]=1;
+                printf("pdu %d bien acquitte - PE=%d\n",pdu_global.header.seq_num,PE);
+                }
+                else 
+                {
+                    printf("BAD Ack received. Taux de réussite actuel: %f \n",perte_admi + sum/10);	
+                }
+                
+            }
+            
+            }
+            while (Timeout==-1 && Retransmettre && nbRetransmit>0);
+
+            // fin  while , on a fini de traiter le Message
+            if (nbRetransmit==0 || (Timeout==-1 && !Retransmettre)) { // Nbre de retransmission max atteint ou perte admissible, marquer paquet perdu
+            //	pertes[PE] deja initialisées à 0 (odouble pertes[10] ={1};n part de l'hypothese d'une perte) on pourra enlever cette condition pour la version optimisee
+            printf("perte admissible ou Nbr retrans max atteint !  Nb Retrans restante: %d,PE=%d ! \n",nbRetransmit,PE);
+
+            }
+            PE=(PE+1)%10;
+            pthread_mutex_lock(&mutex);
+            buf.index=(buf.index+1)%100;
+            buf.nb_messages--;
+            pthread_mutex_unlock(&mutex);
+        } 
+        else
+        {
+            printf("Rien à envoyer tranquille penard easy posey\n");
+            sleep(5);
+        }
+    }
+    
+}
 /*
  * Permet de réclamer l’envoi d’une donnée applicative
  * Retourne la taille des données envoyées, et -1 en cas d'erreur
  */
 int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 {
-  mic_tcp_pdu pdu={0};
-  printf("\n[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-  if (sock.fd==mic_sock)
-    {
-      pdu_global.header.source_port=sock.addr.port;
-      pdu_global.header.dest_port=addr_dest.port;
-      pdu_global.payload.size=mesg_size;
-      pdu_global.payload.data=mesg;
-      pdu_global.header.seq_num=PE;
-      pdu_global.header.ack='0';
-      pdu_global.header.ack_num=-1;
-      pdu_global.header.syn='0';
-      pdu_global.header.fin='0';
-      pertes[PE] = 0;
-      
-      // La regle du stop and wait est qu'une retransmission n'est seulement declenchee qu'au timeout et pas en reaction a une reception d'un PDU inapproprie
-      
-      int Timeout=0; // Timeout=-1 => il y a eu timeout
-      int nbRetransmit=5;
-      // on anticipe la perte pour la prise de déc
-      // precalcul du taux de reussite actuel ..
-      double sum=0;
-      for (int i=0; i<10;i++){
-	    sum+=pertes[i];
-      }
-     
-      int Sent=-1;
-      int Retransmettre=(perte_admi + (sum)/10) < 1;// à 1 => retransmission necessaire 
-      printf("Estimation avant envoi dans le cas où l'on perd le paquet,\nTaux de perte : %f,Taux Livraison:%f/10 et Retransmettre=%d \n",1-sum/10,sum,Retransmettre);
-      
-      // Transmission PDU 
-      Sent=IP_send(pdu_global,addr_dest);
-      do {
-	Timeout=IP_recv(&pdu,&addr_dest,100);
-	if (Timeout == -1) { // il y a eu timeout
-	  if (Retransmettre) {  // il y a necessite de retransmisison 
-	      // Retransmission 
-	      Sent=IP_send(pdu_global,addr_dest);
-	      nbRetransmit--;
-	    }
-	}
-	else // PDU recu
-	  {
-	    if (pdu.header.ack=='1' && pdu.header.ack_num==pdu_global.header.seq_num) { // c'est le bon ACK
-	      pertes[PE]=1;
-	      printf("pdu %d bien acquitte - PE=%d\n",pdu_global.header.seq_num,PE);
-	    }
-	    else 
-	      {
-		    printf("BAD Ack received. Taux de réussite actuel: %f \n",perte_admi + sum/10);	
-	      }
-	    
-	  }
-	
-      }
-      while (Timeout==-1 && Retransmettre && nbRetransmit>0);
-
-      // fin  while , on a fini de traiter le Message
-      if (nbRetransmit==0 || (Timeout==-1 && !Retransmettre)) { // Nbre de retransmission max atteint ou perte admissible, marquer paquet perdu
-	//	pertes[PE] deja initialisées à 0 (odouble pertes[10] ={1};n part de l'hypothese d'une perte) on pourra enlever cette condition pour la version optimisee
-	printf("perte admissible ou Nbr retrans max atteint !  Nb Retrans restante: %d,PE=%d ! \n",nbRetransmit,PE);
-
-      }
-      PE=(PE+1)%10;
-	//        printf("ack received \n");
-
-        return Sent;
+    printf("\n[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+    pthread_mutex_lock(&mutex);
+    if (buf.nb_messages==100){
+        printf("trop de messages en attente (100) attendez\n");
     }
-    return -1;
+    else{
+        buf.nb_messages++;
+        buf.messages[(buf.nb_messages+buf.index)%100].data=mesg;
+        buf.messages[(buf.nb_messages+buf.index)%100].size=mesg_size;
+    }
+    pthread_mutex_unlock(&mutex);
+    return mesg_size;
 }
 
 /*
@@ -240,7 +278,6 @@ int mic_tcp_close (int socket)
  */
 void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
 {
-
   printf("[MIC-TCP] Appel de la fonction (seqnum,acknum) :%d;%d",pdu.header.seq_num,pdu.header.ack_num); printf(__FUNCTION__); printf("\n");
     if (pdu.header.ack=='0' && pdu.header.syn=='0')
     { 
@@ -257,7 +294,6 @@ void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
         } 
         if (  pdu.header.seq_num>=PA)  { 
             app_buffer_put(pdu.payload);
-            printf("[MIC-process] Message attendu recu:%s\n",pdu.payload.data);
         }
         else {
             printf("Mauvais num de seq. Message déjà reçu ou déjà abandonné. Envoi d'un ack pour que l'envoyeur passe au prochain");
@@ -283,5 +319,4 @@ void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
         } 
         pthread_cond_signal(&cond);
     }
-    
 }
